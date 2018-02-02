58db2a170dc4de5ec08bb0045f493f3c1312d7dd
Esben Jacob Sloth <esben@pocketplaylab.com>
2018-02-02 11:41:22 +0700

    Unity Training

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform Player;
+	public float Speed = .5f;
+	private Rigidbody2D rigidbodyComponent;
+	private Vector2 CameraVector;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CameraVector = ( Player.position - transform.position ) * Speed;
+	}
+
+
+	void FixedUpdate() {
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+		rigidbodyComponent.AddForce(CameraVector);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed = 1f;
+	private Vector2 movement;
+	private Rigidbody2D rigidbodyComponent;
+	
+
+	void Update() {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2(
+			inputX,
+			inputY
+		).normalized 
+		* Mathf.Max( 
+				Mathf.Abs( inputX ), 
+				Mathf.Abs( inputY ) 
+		) 
+		* Speed;
+	}
+
+	void FixedUpdate() {
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+		rigidbodyComponent.velocity = movement;
+	}
+}
